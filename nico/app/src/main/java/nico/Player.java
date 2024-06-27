bbafb07fe287fe50da92a9ac8eabfede66b08e8f
nico-arroyo <nicolas.arroyo.geldhof@gmail.com>
2024-06-27 10:19:16 +0200

    Update Player.java

--- nico/app/src/main/java/nico/Player.java
+++ nico/app/src/main/java/nico/Player.java
@@ -2,48 +2,124 @@ package nico;
 
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-
-import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class Player {
-    private String name;
-    private String lastName;
-    private Position position;
-    private ArrayList<String> teams;
-    //private Locale nationality;
-    private PreferredLeg preferredLeg;
-    private Status status;
 
-    public Player(String name, String lastName, Position position, ArrayList<String> teams, PreferredLeg preferredLeg, Status status) {
-        this.name = name;
-        this.lastName = lastName;
-        this.position = position;
-        this.teams = teams;
-        //this.nationality = nationality;
-        this.preferredLeg = preferredLeg;
-        this.status = status;
+    private final UUID id;
+    private final String name;
+    private final String lastName;
+    private final Position position;
+    private final List<String> teams;
+    private final String nationality;
+    private final PreferredLeg preferredLeg;
+    private final Status status;
+    private final UUID teamId;
+
+    private Player(Builder builder) {
+        id = builder.id;
+        name = builder.name;
+        lastName = builder.lastName;
+        position = builder.position;
+        teams = builder.teams;
+        nationality = builder.nationality;
+        preferredLeg = builder.preferredLeg;
+        status = builder.status;
+        teamId = builder.teamId;
     }
 
-    public String getName() { return name; }
-    public String getLastName() { return lastName; }
-    public Position getPosition() { return position; }
-    public ArrayList<String> getTeams() { return teams; }
-    //public Locale getNationality() { return nationality; }
-    public PreferredLeg getPreferredLeg() { return preferredLeg; }
-    public Status getStatus() { return status; }
-
-    public String toString() {
-        return String.format("{name: %s, lastName: %s, position: %s, teams: %s, preferredLeg: %s, status: %s}",
-                getName(), getLastName(), getPosition(), getTeams(), getPreferredLeg(), getStatus());
+    public static Player create(JsonObject data) {
+        return Builder.builder()
+                .withId(UUID.randomUUID())
+                .withName(data.getString("name"))
+                .withLastName(data.getString("last_name"))
+                .withPosition(Position.valueOf(data.getString("position")))
+                .withTeams(data.getJsonArray("clubs").getList())
+                .withNationality(data.getString("nationality"))
+                .withPreferredLeg(PreferredLeg.valueOf(data.getString("preferred_leg")))
+                .withStatus(Status.valueOf(data.getString("status")))
+                .withTeamId(UUID.fromString(data.getString("teamId")))
+                .build();
     }
 
     public JsonObject toJson() {
         return new JsonObject()
+                .put("id", id.toString())
                 .put("name", name)
-                .put("lastName", lastName)
+                .put("last_name", lastName)
                 .put("position", position)
                 .put("clubs", new JsonArray(teams))
-                .put("preferredLeg", preferredLeg)
-                .put("status", status);
+                .put("nationality", nationality)
+                .put("preferred_leg", preferredLeg)
+                .put("status", status)
+                .put("teamId", teamId);
+    }
+
+    public static final class Builder {
+        private UUID id;
+        private String name;
+        private String lastName;
+        private Position position;
+        private List<String> teams;
+        private String nationality;
+        private PreferredLeg preferredLeg;
+        private Status status;
+        private UUID teamId;
+
+        private Builder() {}
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder withId(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder withPosition(Position position) {
+            this.position = position;
+            return this;
+        }
+
+        public Builder withTeams(List<String> teams) {
+            this.teams = teams;
+            return this;
+        }
+
+        public Builder withNationality(String nationality) {
+            this.nationality = nationality;
+            return this;
+        }
+
+        public Builder withPreferredLeg(PreferredLeg preferredLeg) {
+            this.preferredLeg = preferredLeg;
+            return this;
+        }
+
+        public Builder withStatus(Status status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder withTeamId(UUID teamId) {
+            this.teamId = teamId;
+            return this;
+        }
+
+        public Player build() {
+            return new Player(this);
+        }
     }
 }
